78589b2329af22250774475cfd21cd77f02553f2
Stadler <sebastian.stadler@ibp.fraunhofer.de>
2020-06-22 12:39:36 +0200

    added fraunhofer label for date x-axis

---
+++ R/geom_fraunhofer_label_date.R
@@ -0,0 +1,20 @@
+#' Title
+#'
+#' @param ...
+#' @param angle Winkel in dem der Schriftzug angezeigt wird, Standard 90 Grad.
+#' @param hjust Horizontale Ausrichtung vom Ankerpunkt. Wir genutzt BEVOR der Text gedreht wird.
+#' @param vjust Vertikale Ausrichtung vom Ankerpunkt. Wir genutzt BEVOR der Text gedreht wird.
+#' @param x_pos Setzt den Ankerpunkt bei min(x) (x_pos==-1) oder max(x) (x_pos==1)
+#' @param y_pos Setzt den Ankerpunkt bei min(y) (y_pos==-1) oder max(y) (y_pos==1)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+geom_fraunhofer_label_date <- function(..., angle = 90, hjust = -0.05, vjust = -0.5, x_pos = 1, y_pos = -1) {
+  ggplot2::geom_text(
+    data = data.frame("label" = "© Fraunhofer IBP", stringsAsFactors = F),
+    aes(label = label, x = structure(x_pos*Inf, class = "Date"), y = y_pos * Inf), angle = angle, size = 2.7,
+    hjust = hjust, vjust = vjust, fontface = "bold", ...
+  )
+}
