efac169d7eba1ba4fc3db0b5195284bfaf9439ec
Stadler <sebastian.stadler@ibp.fraunhofer.de>
2020-06-25 15:39:00 +0200

    improved get_fitting_borders by allowing more values

--- R/get_fitting_borders.R
+++ R/get_fitting_borders.R
@@ -16,14 +16,18 @@ get_fitting_borders <- function(value, direction = 1, leading = 1){
     return(value)
   } else {
 
+
   if(abs(value)>1){
+    if(nchar(value)-1 <= leading){
+      leading <- nchar(value) - 1
+    }
   var1 <- value/(10**(nchar(round(value,0))-leading))
-    if(direction==1){
+    if(direction> 0 ){
       border <- ceiling(var1)*(10**(nchar(round(value,0))-leading))
-    } else if(direction==-1){
+    } else if(direction < 0){
       border <- floor(var1)*(10**(nchar(round(value,0))-leading))
     } else {
-      print("Please only use 1 or -1 for direction. Round was used")
+      print("Please only use positive or negative values for direction. Round was used")
       border <- round(var1)*(10**(nchar(round(value,0))-leading))
     }
   } else {
