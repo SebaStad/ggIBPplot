c6afcdaac5a2e40c909ef3757575491fd987abcb
Stadler <sebastian.stadler@ibp.fraunhofer.de>
2020-09-25 14:14:33 +0200

    added scale_y_date and scale_y_datime

---
+++ R/scale_y_ibp_date.R
@@ -0,0 +1,26 @@
+#' Title
+#'
+#' @param n Anzahl von Major Gridlines
+#' @param limits Vector mir minimal und maximal Wert
+#' @param remove_border_val Derzeit nicht benutzt
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+scale_y_ibp_date <- function(n = 7, limits = NULL, remove_border_val = FALSE, ...) {
+  if (is.null(limits)) {
+    ggplot2::scale_y_date(breaks = equal_breaks(n = n, s = 0,
+                                                remove_border_val = remove_border_val),
+                          expand = c(0, 0), ...)
+  } else {
+    ggplot2::scale_y_date(
+      breaks = equal_breaks(
+        n = n, s = 0, manual = limits,
+        remove_border_val = remove_border_val
+      ),
+      expand = c(0, 0), limits = limits, ...
+    )
+  }
+}
