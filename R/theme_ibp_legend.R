b39ac474305d5417f49520e259d4b37a203b01a5
Stadler <sebastian.stadler@ibp.fraunhofer.de>
2020-11-11 17:21:13 +0100

    split theme_ibp and theme_ibp_legend

--- R/theme_ibp_legend.R
+++ R/theme_ibp_legend.R
@@ -14,11 +14,12 @@
 #' @export
 #'
 #' @examples
-theme_ibp <- function(base_size = 11,
+theme_ibp_legend <- function(base_size = 11,
                       base_family = "",
                       base_line_size = base_size / 170,
                       base_rect_size = base_size / 170,
-                    #  aspect = 5.5 / 8,
+                      x_pos_leg = 0.1, y_pos_leg = 0.85,
+                      #  aspect = 5.5 / 8,
                       face = "plain",
                       ibp_family = "Frutiger 45 Light" ) {
   ggplot2::theme_minimal(
@@ -31,20 +32,20 @@ theme_ibp <- function(base_size = 11,
       plot.title = ggplot2::element_text(
         colour = "black",
         family = ibp_family,
-     #   face = face,
+        #   face = face,
         hjust = 0
       ),
       axis.title = ggplot2::element_text(
         colour = "black",
         family = ibp_family,
-     #   face = face,
+        #   face = face,
         size = rel(1)
       ),
       axis.text = ggplot2::element_text(
         colour = "black",
         family = ibp_family,
-    #    face = face,
-    #    family = font,
+        #    face = face,
+        #    family = font,
         size = rel(0.9)
       ),
       panel.grid.major = ggplot2::element_line(
@@ -53,7 +54,10 @@ theme_ibp <- function(base_size = 11,
       panel.grid.minor = ggplot2::element_blank(),
       legend.title = ggplot2::element_text(hjust = 0, face = face, rel(0.9)),
       legend.text = ggplot2::element_text(face = face, rel(0.9)),
+      legend.position = c(x_pos_leg, y_pos_leg),
       legend.background = ggplot2::element_rect(colour = "white"),
+      #    aspect.ratio = aspect,
+
       complete = TRUE
     )
 }
--- tests/testthat/test-coord.R
+++ tests/testthat/test-coord.R
@@ -1,46 +1,47 @@
 library(ggplot2)
 library(rlang)
 
-test_that("coord_ibp_cartesion is a ggproto object",{
+test_that("coord_ibp_cartesion is a ggproto object", {
   expect_true(is.ggproto(ggIBPplot::coord_ibp_cartesian()))
 })
 
-test_that("limits are given correctly",{
+test_that("limits are given correctly", {
   p <- ggplot(mtcars, aes(x = mpg, y = hp)) +
     geom_point() +
     scale_x_ibp_cont(n = 5) +
-    scale_y_ibp_cont(n= 5) +
+    scale_y_ibp_cont(n = 5) +
     coord_ibp_cartesian()
 
-  expect_equal(eval(call2(layer_scales(p)$x$limits, x = layer_scales(p)$x$range$range)),
-               c(10,34))
+  expect_equal(
+    eval(call2(layer_scales(p)$x$limits, x = layer_scales(p)$x$range$range)),
+    c(10, 34)
+  )
 
-  expect_equal(eval(call2(layer_scales(p)$y$limits, x = layer_scales(p)$y$range$range)),
-               c(50,400))
-
-
-
+  expect_equal(
+    eval(call2(layer_scales(p)$y$limits, x = layer_scales(p)$y$range$range)),
+    c(50, 400)
+  )
 })
 
 
-test_that("breaks are given correctly",{
+test_that("breaks are given correctly", {
   p <- ggplot(mtcars, aes(x = mpg, y = hp)) +
     geom_point() +
     scale_x_ibp_cont(n = 5) +
-    scale_y_ibp_cont(n= 5) +
+    scale_y_ibp_cont(n = 5) +
     coord_ibp_cartesian()
 
-  expect_equal(eval(call2(layer_scales(p)$x$breaks,
-                          x = eval(call2(layer_scales(p)$x$limits, x = layer_scales(p)$x$range$range)))),
-               c(10, 16, 22, 28, 34))
+  expect_equal(
+    eval(call2(layer_scales(p)$x$breaks,
+      x = eval(call2(layer_scales(p)$x$limits, x = layer_scales(p)$x$range$range))
+    )),
+    c(10, 16, 22, 28, 34)
+  )
 
-  expect_equal(eval(call2(layer_scales(p)$y$breaks,
-                          x = eval(call2(layer_scales(p)$y$limits, x = layer_scales(p)$y$range$range)))),
-               c(50.0 ,137.5, 225.0, 312.5, 400.0))
-
-
-
+  expect_equal(
+    eval(call2(layer_scales(p)$y$breaks,
+      x = eval(call2(layer_scales(p)$y$limits, x = layer_scales(p)$y$range$range))
+    )),
+    c(50.0, 137.5, 225.0, 312.5, 400.0)
+  )
 })
-
-
-
